04bbb372a998d07bd5f724fcbe76fe1ea0017009
Erik Thomas <erik@brightthought.co>
2019-03-14 16:16:27 -0400

    Update to add user to list

--- mail.php
+++ mail.php
@@ -50,9 +50,41 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 $err = curl_error($curl);
 
+curl_close($curl);
+
 if($err){
 	echo "false";
 	exit();
+}else{
+	$response = json_decode($response, true);
+
+	$list_url = "https://cloudpcrtest.user.com/api/public/users/{$response['id']}/add_to_list/";
+	$list_curl = curl_init($list_url);
+
+	$list_data = ['list' => 1];
+
+	curl_setopt_array($list_curl, array(
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "POST",
+		CURLOPT_POSTFIELDS => json_encode($list_data),
+		CURLOPT_HTTPHEADER => array(
+			"authorization: Token TR5Of4PN9qzowKnyONRAfKQZFBF76ybekYyvwvFrZBxJa09VPGTEfcPHCuClSC11",
+			"content-type: application/json"
+		),
+	));
+
+	$list_response = curl_exec($list_curl);
+	$err = curl_error($list_curl);
+	curl_close($list_curl);
+
+	if($err){
+		echo "false";
+		exit();
+	}
 }
 
 echo 'true';
