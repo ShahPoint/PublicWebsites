285b74ed293465cf6f26ed41280741120b99eecc
Ryan Tedeschi <ryanat13@gmail.com>
2019-03-15 10:58:52 -0500

    More modifications to signup code

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -20,7 +20,7 @@ function getUrlParameter(name) {
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
-function SubmitCoreInfo() {
+function SubmitCoreInfo($form) {
     let $form = $("#signupForm")
     let validator = $form.validate();
 
@@ -30,14 +30,14 @@ function SubmitCoreInfo() {
         // formData.leadContext = getUrlParameter("leadContext");
         // formData.demo = true;
         // formData.testing = true;
-        let name = $("[name=name]").val().trim().split(" ");
+        let name = $("[name=name]", $form).val().trim().split(" ");
         
         let formData = {
             leadOrigin: getUrlParameter("leadOrigin"),
             leadContext: getUrlParameter("leadContext"),
             demo: true,
-            emailAddress: $("[name=email]").val(),
-            phoneNumber: $("[name=phone]").val(),
+            emailAddress: $("[name=email]", $form).val(),
+            phoneNumber: $("[name=phone]", $form).val(),
             firstName: name[0],
             lastName: name[name.length - 1]
         };
